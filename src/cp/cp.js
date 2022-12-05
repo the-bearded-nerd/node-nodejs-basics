8954695523c5991b44e8f2819434e372e4f8a7bb
the-bearded-nerd <dmitrykarvonen@gmail.com>
2022-12-05 22:19:10 +0300

    fix:fix cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,7 +1,13 @@
 import { fork } from "child_process";
+import * as url from "url";
+import * as path from "path";
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+const SCRIPT_NAME = "script.js";
+const pathToScript = path.join(__dirname, "files", SCRIPT_NAME);
 
 const spawnChildProcess = async (args) => {
-  const child = fork("./files/script.js", args);
+  const child = fork(pathToScript, args);
 };
 
 spawnChildProcess();
